4dad7f9dc88ff30369b0ba868606c64f6242e61d
Pedro Lopes <75954029+httpspedroh@users.noreply.github.com>
2023-02-27 10:23:56 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demaaaao',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+		title: 'Flutter Demaaaao',
+		theme: ThemeData.dark(
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -104,20 +94,26 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+
+	  floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: Row(
 
-		mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+		mainAxisAlignment: MainAxisAlignment.spaceAround,
 		children: [
 
 			FloatingActionButton(
+		
 				onPressed: _incrementCounter,
 				tooltip: 'Increment',
+				backgroundColor: Colors.green,
 				child: const Icon(Icons.add),
+				
 			),
 
 			FloatingActionButton(
 				onPressed: _decrementCounter,
 				tooltip: 'Decrement',
+				backgroundColor: Colors.red,
 				child: const Icon(Icons.remove),
 			),
 		],
