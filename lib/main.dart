2eb54241f797e2aad95fd249fa49ea3b7ae91ecf
Victor Lopes <victor.araujo.1348805@sga.pucminas.br>
2023-03-28 18:34:31 -0300

    victor

--- lib/main.dart
+++ lib/main.dart
@@ -1,95 +1,75 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  	runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-	const MyApp({super.key});
-
-	@override
-	Widget build(BuildContext context) {
-
-		return MaterialApp(
-
-			title: 'Flutter Demo',
-			debugShowCheckedModeBanner: false,
-			theme: ThemeData.light(),
-			home: const MyHomePage(title: 'Flutter Demo Home Page'),
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page 2'),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
-  	const MyHomePage({super.key, required this.title});
+  final String title;
 
-  	final String title;
-
-	@override
-	State<MyHomePage> createState() => _MyHomePageState();
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-	int _counter = 0;
-  
-	void _decrementCounter() {
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
 
-		setState(() {
-			_counter--;
-		});
-	}
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
-	void _incrementCounter() {
-
-		setState(() {
-			_counter++;
-		});
-	}
-
-	@override
-	Widget build(BuildContext context) {
-		
-		return Scaffold(
-
-			appBar: AppBar(
-				title: Text(widget.title),
-			),
-
-			body: Container(
-				child: SizedBox.expand(
-					child: Image.asset("assets/images/gato.jpg",
-						fit: BoxFit.cover
-					),
-				),
-			),
-
-			floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-			floatingActionButton: Row(
-
-				mainAxisAlignment: MainAxisAlignment.spaceAround,
-				children: [
-
-					FloatingActionButton(
-				
-						onPressed: _incrementCounter,
-						tooltip: 'Increment',
-						backgroundColor: Colors.green,
-						child: const Icon(Icons.add),
-						
-					),
-
-					FloatingActionButton(
-
-						onPressed: _decrementCounter,
-						tooltip: 'Decrement',
-						backgroundColor: Colors.red,
-						child: const Icon(Icons.remove),
-					),
-				],
-			)
-		);
-	}
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Container(
+          child: SizedBox.expand(
+            child: Image.asset("assets/images/gato.jpg", fit: BoxFit.cover),
+          ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Increment',
+              backgroundColor: Colors.green,
+              child: const Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              onPressed: _decrementCounter,
+              tooltip: 'Decrement',
+              backgroundColor: Colors.red,
+              child: const Icon(Icons.remove),
+            ),
+          ],
+        ));
+  }
 }
